523750f1826bc10b36b95b3c24349566b9461d9d
cesarmezab <99105052+CesarMezaB@users.noreply.github.com>
2022-04-25 11:30:42 -0700

    Add my first app with express

---
+++ app.js
@@ -0,0 +1,18 @@
+// Usando objeto express
+const express = require('express')
+//App de Express
+const app = express()
+// Puerto en que se ca a ver la app: localhost:3000
+const port = 3000
+
+// Path inicial, respondereea a la url localhost:3000
+app.get('/', (req, res) => {
+    res.send('Hello Wordl!')
+})
+
+
+// Con esto inicializamos esta app 
+
+app.listen(port, () =>{
+    console.log(`Example app listening on port ${port}`)
+    })
